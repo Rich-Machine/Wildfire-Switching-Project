a29a6cd8794c3694b8d74db760cda0819f20ce1f
Amanda West <38090325+awest32@users.noreply.github.com>
2024-02-26 11:47:30 -0500

    wildfire_psps_game

---
+++ test.jl
@@ -0,0 +1,9 @@
+#using WildfirePSPS_Game
+using Test
+
+#@testset "WildfirePSPS_Game.jl" begin
+ #   @test WildfirePSPS_Game.greet_WildfirePSPS_Game() == "Hello WildfirePSPS_Game!"
+  #  @test WildfirePSPS_Game.greet_WildfirePSPS_Game() != "Hello world!"
+#end
+
+print(WildfirePSPS_Game.lp_solve())
