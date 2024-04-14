fa2ed18d3f4d1ebb93f83b4bbd51fe4c0d78f8e6
Richard Asiamah <123144205+Rich-Machine@users.noreply.github.com>
2024-04-14 18:41:12 -0400

    Update and rename Plotting_networks.jl to plotting_networks.jl

--- plotting_networks.jl
+++ plotting_networks.jl
@@ -1,19 +1,15 @@
-using JuMP
-using HiGHS
-using Gurobi
-using Flux
-using BSON
 using PowerPlots
 using PowerModels
 
-nn_model = BSON.load("wildfire_trained_model.bson")
-eng = PowerModels.parse_file("case5_risk.m")
+network_type = "base_case"
+# network_type = "sole_gen"
+# network_type = "high_risk"
+
+eng = PowerModels.parse_file("case5_risk_$network_type.m")
 
 p = powerplot(eng,
-# bus_data=:bus_status, bus_data_type=:quantitative,
 branch_data=:power_risk, 
 branch_data_type=:quantitative,
-# branch_color=["#4169f1", "#4169f1","red"],
 branch_color=["#4169f1","red"],
 gen_data=:gen_status, gen_data_type=:ordinal, gen_color=["orange", "black"],
 height=400, width=420, bus_size=150, gen_size=100, load_size=50,
