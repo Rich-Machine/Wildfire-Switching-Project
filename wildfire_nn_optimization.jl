2458b7edbe7f76a146af1ba43f3acc23408da1d7
Richard Asiamah <123144205+Rich-Machine@users.noreply.github.com>
2024-04-21 19:08:39 -0400

    Update wildfire_nn_optimization.jl

--- wildfire_nn_optimization.jl
+++ wildfire_nn_optimization.jl
@@ -28,8 +28,11 @@ line_6 = []
 # Define alpha parameter
 #alpha = (0.01, 0.02, 0.03, 0.04, 0.05, 0.06,  0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16,  0.17, 0.18, 0.19, 0.2)
 #if network_type == "sole_gen"
-    alpha = (0.1, 0.2, 0.3, 0.4, 0.5, 0.6,  0.7, 0.8, 0.9)
+    #alpha = (0.1, 0.2, 0.3, 0.4, 0.5, 0.6,  0.7, 0.8, 0.9)
 #end
+
+alpha = (0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.19, 0.21, 0.23, 0.25, 0.27, 0.29, 0.31, 0.33, 0.35, 0.37, 0.39, 0.41, 0.43, 0.45, 0.47, 0.49, 0.51, 0.53, 0.55, 0.57, 0.59, 0.61, 0.63, 0.65, 0.67, 0.69, 0.71, 0.73, 0.75, 0.77, 0.79, 0.81, 0.83, 0.85, 0.87, 0.89, 0.91, 0.93, 0.95, 0.97, 0.99)
+
 
 for j in alpha
     print("This is $j\n \n")
@@ -117,9 +120,9 @@ for j in alpha
     optimize!(model)    
 
     push!(objective, JuMP.objective_value(model))
-    push!(load_shed_units, JuMP.value.(x3)/D_p)
+    push!(load_shed_units, (JuMP.value.(x3)/D_p)*100)
     line_risk = sum(risk[i] * JuMP.value.(line_status)[i] for i in 1:6)
-    push!(wildfire_risk, line_risk/total_risk)
+    push!(wildfire_risk, (line_risk/total_risk)*100)
     push!(line_1, JuMP.value.(line_status)[1])
     push!(line_2, JuMP.value.(line_status)[2])
     push!(line_3, JuMP.value.(line_status)[3])
@@ -139,13 +142,13 @@ for j in alpha
     end
 end 
 
-alpha = range(0.01, 0.2, length=20)
-if network_type == "sole_gen"
-    alpha = range(0.1, 1, length=9)
-end
-
+# alpha = range(0.01, 0.2, length=20)
+# if network_type == "sole_gen"
+#     alpha = range(0.1, 1, length=9)
+# end
+alpha = range(1, 99, length = 50)
 load_shed_units_combined = vcat(load_shed_units...)
-plot(load_shed_units_combined, wildfire_risk, title = "Total Risk VS Total Load Shed for $network_type", xlabel = "% of load shed", ylabel = "Total Wildfire risk", label="Total Risk")
+plot!(load_shed_units_combined, wildfire_risk, title = "Wildfire risk VS load shed for all networks", xlabel = "Percentage of load shed", ylabel = "Percentage of wildfire risk", label="High Risk")
 
 ## Plotting the heat wave for the line statuses
 # Define the lines
